6d35e8d7f7d78500f40930ca5ba9fd1142cd2623
毛财君 <mccj@qq.com>
2023-04-11 14:18:03 +0800

    1

--- src/HttpReportsDashboardServer/Program.cs
+++ src/HttpReportsDashboardServer/Program.cs
@@ -1,3 +1,8 @@
+using HttpReportsDashboardServer;
+using LogDashboard;
+using LogDashboard.Extensions;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
@@ -25,9 +30,33 @@ switch (storageDBType)
         break;
 }
 
+builder.Services.AddHttpClient();
+builder.Services.AddHealthChecks()
+//.AddSqlServer(builder.Configuration["ConnectionStrings:connectionString"]);
+;
+
+builder.Services.AddLogDashboard(opt =>
+{
+    var username = builder.Configuration.GetValue<string>("LogDashboard:UserName");
+    var password = builder.Configuration.GetValue<string>("LogDashboard:PassWord") ?? "";
+    var pathMatch = builder.Configuration.GetValue<string>("LogDashboard:PathMatch");
+    var brand = builder.Configuration.GetValue<string>("LogDashboard:Brand");
+
+    if (!string.IsNullOrWhiteSpace(username))
+        opt.AddAuthorizationFilter(new LogDashboardBasicAuthorizationFilter(username, password));
+    if (!string.IsNullOrWhiteSpace(pathMatch)) opt.PathMatch = pathMatch;
+    if (!string.IsNullOrWhiteSpace(brand)) opt.Brand = brand;
+
+    opt.SetRootPath(System.IO.Path.Combine(AppContext.BaseDirectory + "App_Data", "logs"));
+    opt.CustomLogModel<LogDashboard.Models.RequestTraceLogModel>();
+});
+builder.Host.UseNLog();
+
 var app = builder.Build();
 
 app.UseHttpReportsDashboard();
+app.UseLogDashboard();
+app.UseHealthChecks("/health");
 
 app.Run();
 
